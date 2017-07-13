12d6fabb8864e339be1d6ffeba58ab89bd294d97
Heiner Wolf <wolf.heiner@gmail.com>
2017-07-13 15:24:41 +0200

    add the relevant stuff

---
+++ as.ts
@@ -0,0 +1,129 @@
+﻿interface As
+{
+    Bool(val: any, alt?: boolean): boolean;
+    String(val: any, alt?: string): string;
+    Int(val: any, alt?: number): number;
+    Float(val: any, alt?: number): number;
+    Html(val: any, alt?: string): string;
+    HtmlLink(val: any, text?: string, urlFilter?: (s: string) => string, alt?: string): string;
+    Object(val: any, alt?: string): any;
+}
+
+var as: As = (function ()
+{
+    var escapeHtml_entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        "'": '&quot;',
+        '"': '&#39;',
+    };
+
+    var typeString = 'string';
+    var typeBoolean = 'boolean';
+    var typeNumber = 'number';
+
+    return {
+        Bool: function (val, alt = false): boolean
+        {
+            var res = alt;
+            if (typeof val === typeBoolean) {
+                res = val;
+            } else {
+                if (typeof val === typeString) {
+                    if (val == 'true' || val == 'True' || val == 'TRUE' || val == '1' || val == 'yes') { res = true; }
+                } else {
+                    if (typeof val === typeNumber) {
+                        if (val == 1) { res = true; }
+                    }
+                }
+            }
+            return res;
+        },
+
+        String: function (val, alt = ''): string
+        {
+            var res = alt;
+            if (typeof val === typeString) {
+                res = val;
+            } else {
+                if (typeof val === typeNumber) {
+                    res = '' + val;
+                } else {
+                    if (typeof val === typeBoolean) {
+                        res = val ? 'true' : 'false';
+                    }
+                }
+            }
+            return res;
+        },
+
+        Int: function (val, alt = 0): number
+        {
+            var res = alt;
+            if (typeof val === typeNumber) {
+                res = Math.round(val);
+            } else {
+                if (typeof val === typeString) {
+                    res = parseInt(val);
+                    if (isNaN(res)) {
+                        res = alt;
+                    }
+                }
+            }
+            return res;
+        },
+
+        Float: function (val, alt = 0.0): number
+        {
+            var res = alt;
+            if (typeof val === typeNumber) {
+                res = val;
+            } else {
+                if (typeof val === typeString) {
+                    res = parseFloat(val);
+                    if (isNaN(res)) {
+                        res = alt;
+                    }
+                }
+            }
+            return res;
+        },
+
+        Html: function (val, alt = ''): string
+        {
+            var res = as.String(val, alt);
+            return String(res).replace(/[&<>'"]/g, (s) => escapeHtml_entityMap[s]);
+        },
+
+        HtmlLink: function (val, text = '', urlFilter: (s: string) => string = null, alt = ''): string
+        {
+            var res = as.String(val, alt);
+            if (urlFilter == null) {
+                urlFilter = (s => s.substr(0, 4) == 'http' ? s : '');
+            }
+            var url = urlFilter(res);
+            if (as.String(url) != '') {
+                if (text == '') {
+                    text = url;
+                }
+                res = '<a href="' + as.Html(url) + '">' + as.Html(text) + '</a>'
+            }
+            return res;
+        },
+
+        Object: function (val, alt = ''): any
+        {
+            var res = as.String(val, alt);
+            var obj = null;
+            try {
+                obj = JSON.parse(res);
+            } catch (exception) {
+                obj = JSON.parse(alt);
+            }
+            return obj;
+        },
+
+        _last: 0
+    }
+})();
---
+++ qunit-tests.js
@@ -0,0 +1,101 @@
+﻿QUnit.test('as.Bool', function (assert) {
+    assert.ok(as.Bool(true) == true);
+    assert.ok(as.Bool(false) == false);
+    assert.ok(as.Bool('') == false);
+    assert.ok(as.Bool('true') == true);
+    assert.ok(as.Bool('1') == true);
+    assert.ok(as.Bool(1) == true);
+    assert.ok(as.Bool(undefined) == false);
+    assert.ok(as.Bool(null) == false);
+    assert.ok(as.Bool({ a: 'b' }.c) == false);
+    assert.ok(as.Bool(true, false) == true);
+    assert.ok(as.Bool(false, true) == false);
+    assert.ok(as.Bool('', true) == true);
+    assert.ok(as.Bool('true', false) == true);
+    assert.ok(as.Bool('1', false) == true);
+    assert.ok(as.Bool(1, false) == true);
+    assert.ok(as.Bool(undefined, true) == true);
+    assert.ok(as.Bool(null, true) == true);
+    assert.ok(as.Bool({ a: 'b' }.c, true) == true);
+    assert.ok(as.Bool({ a: 'b' }['c'], true) == true);
+    assert.ok(as.Bool(as.String(true)) == true);
+    assert.ok(as.Bool(as.String(false)) == false);
+    assert.ok(as.String(as.Bool('true')) == 'true');
+    assert.ok(as.String(as.Bool('false')) == 'false');
+});
+
+QUnit.test('as.String', function (assert) {
+    assert.ok(as.String('fourtytwo') == 'fourtytwo');
+    assert.ok(as.String('') == '');
+    assert.ok(as.String(42) == '42');
+    assert.ok(as.String(undefined) == '');
+    assert.ok(as.String(null) == '');
+    assert.ok(as.String({ a: 'b' }.c) == '');
+    assert.ok(as.String('fourtytwo', 'default') == 'fourtytwo');
+    assert.ok(as.String('', 'default') == '');
+    assert.ok(as.String(42, 'default') == '42');
+    assert.ok(as.String(undefined, 'default') == 'default');
+    assert.ok(as.String(null, 'default') == 'default');
+    assert.ok(as.String({ a: 'b' }.c, 'default') == 'default');
+    assert.ok(as.String({ a: 'b' }['c'], 'default') == 'default');
+});
+
+QUnit.test('as.Int', function (assert) {
+    assert.ok(as.Int(42) == 42);
+    assert.ok(as.Int(42.1) == 42);
+    assert.ok(as.Int('') == 0);
+    assert.ok(as.Int('fourtytwo') == 0);
+    assert.ok(as.Int('42') == 42);
+    assert.ok(as.Int('42.1') == 42);
+    assert.ok(as.Int(undefined) == 0);
+    assert.ok(as.Int(null) == 0);
+    assert.ok(as.Int({ a: 'b' }.c) == 0);
+    assert.ok(as.Int(42, 41) == 42);
+    assert.ok(as.Int(42.1, 41) == 42);
+    assert.ok(as.Int('', 41) == 41);
+    assert.ok(as.Int('fourtytwo', 41) == 41);
+    assert.ok(as.Int('42', 41) == 42);
+    assert.ok(as.Int('42.1', 41) == 42);
+    assert.ok(as.Int(undefined, 41) == 41);
+    assert.ok(as.Int(null, 41) == 41);
+    assert.ok(as.Int({ a: 'b' }.c, 41) == 41);
+});
+
+QUnit.test('as.Float', function (assert) {
+    assert.ok(as.Float(42) == 42);
+    assert.ok(as.Float(42.1) == 42.1);
+    assert.ok(as.Float('') == 0);
+    assert.ok(as.Float('fourtytwo') == 0);
+    assert.ok(as.Float('42') == 42);
+    assert.ok(as.Float('42.1') == 42.1);
+    assert.ok(as.Float(undefined) == 0);
+    assert.ok(as.Float(null) == 0);
+    assert.ok(as.Float({ a: 'b' }.c) == 0);
+    assert.ok(as.Float(42, 3.14) == 42);
+    assert.ok(as.Float(42.1, 3.14) == 42.1);
+    assert.ok(as.Float('', 3.14) == 3.14);
+    assert.ok(as.Float('fourtytwo', 3.14) == 3.14);
+    assert.ok(as.Float('42', 3.14) == 42);
+    assert.ok(as.Float('42.1', 3.14) == 42.1);
+    assert.ok(as.Float(undefined, 3.14) == 3.14);
+    assert.ok(as.Float(null, 3.14) == 3.14);
+    assert.ok(as.Float({ a: 'b' }.c, 3.14) == 3.14);
+});
+
+QUnit.test('as.Html', function (assert) {
+    assert.ok(as.Html('fourtytwo') == 'fourtytwo');
+    assert.ok(as.Html('&') == '&amp;');
+    assert.ok(as.Html(null, '&') == '&amp;');
+    assert.ok(as.Html('') == '');
+});
+
+QUnit.test('as.Object', function (assert) {
+    assert.ok(as.Object('{ "answer" : 42 }').answer == 42);
+    assert.ok(as.Object(null, '{ "answer" : 42 }').answer == 42);
+    assert.ok(as.Object('[ "one", "two" ]').length == 2);
+    assert.ok(as.Object('true'));
+    assert.ok(as.Object('42') == 42);
+    assert.ok(as.Object('"fourtytwo"') == 'fourtytwo');
+    assert.ok(as.Object('{', '42') == '42');
+});
+
